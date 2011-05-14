c5bb98b80449ba60452e8883d4ab986f23fbdd0c
Nicolas Martyanoff <khaelin@gmail.com>
2011-05-14 09:11:59 +0200

    remove useless and wrong http response in examples

--- examples/test.lisp
+++ examples/test.lisp
@@ -7,6 +7,7 @@
        (test-request-process handler (m2cl:handler-receive-json handler)))))
 
 (defun test-request-process (handler request)
+  (declare (ignore handler))
   (format t "~&message~%")
   (format t "  sender: ~A~%" (m2cl:request-sender request))
   (format t "  connection-id: ~A~%" (m2cl:request-connection-id request))
@@ -15,6 +16,4 @@
   (dolist (header (m2cl:request-headers request))
     (format t "    ~A: ~A~%" (car header) (cdr header)))
   (format t "  body: ~A~%" (m2cl:request-body request))
-  (format t "  data: ~A~%" (m2cl:request-data request))
-  (m2cl:handler-reply-http handler request (format nil "{}~%")
-                      :headers '(("Content-Type" . "text/javascript"))))
+  (format t "  data: ~A~%" (m2cl:request-data request)))
